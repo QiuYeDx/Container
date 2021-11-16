8109dcfc7e0195ef58b004bad12d73c60ec80d8f
C-B-Elite <czgelite@163.com>
2021-11-17 00:03:31 +0800

    Initial commit.

---
+++ src/Container_assets/src/index.js
@@ -0,0 +1,9 @@
+import { Container } from "../../declarations/Container";
+
+document.getElementById("clickMeBtn").addEventListener("click", async () => {
+  const name = document.getElementById("name").value.toString();
+  // Interact with Container actor, calling the greet method
+  const greeting = await Container.greet(name);
+
+  document.getElementById("greeting").innerText = greeting;
+});
